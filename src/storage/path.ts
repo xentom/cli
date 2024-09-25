8469cb3cfac7c9f116beed098d07b188ff52003e
Tobias Mose <mosetobias@gmail.com>
2024-09-25 19:35:47 +0200

    Handle unknown home directory

--- src/storage/path.ts
+++ src/storage/path.ts
@@ -1,12 +1,22 @@
 import * as os from 'node:os';
 import * as path from 'node:path';
 import { env } from '@/env';
+import { ActionError, catchError } from '@/utils/action';
 
 export function getGlobalConfigPath() {
-  const { hostname } = new URL(env.XENTOM_ADDRESS);
-  if (hostname !== 'xentom.com') {
-    return path.join(os.homedir(), '.xentom', hostname);
+  const homedir = os.homedir();
+  if (homedir === 'unknown') {
+    catchError(
+      new ActionError(
+        'Unable to determine the home directory. If you are using Turbo, please ensure that you add the --env-mode=loose flag to include the system environment variables.',
+      ),
+    );
   }
 
-  return path.join(os.homedir(), '.xentom');
+  const { hostname } = new URL(env.XENTOM_ADDRESS);
+  if (hostname !== 'xentom.com') {
+    return path.join(homedir, '.xentom', hostname);
+  }
+
+  return path.join(homedir, '.xentom');
 }
